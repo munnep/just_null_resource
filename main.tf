ca18a78f8b834e430d3f70db60283d78a6299870
munnep <patrick.munne@hashicorp.com>
2023-07-31 15:37:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,4 @@ resource "null_resource" "test5" {}
 resource "null_resource" "test6" {}
 resource "null_resource" "test7" {}
 resource "null_resource" "test8" {}
+resource "null_resource" "test9" {}
