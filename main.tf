d4471150481dc89457eec922ce62ebd766194441
munnep <patrick.munne@hashicorp.com>
2023-10-16 12:31:35 +0200

    aya and patrick test

--- main.tf
+++ main.tf
@@ -10,6 +10,9 @@ resource "null_resource" "test5" {}
 resource "null_resource" "test6" {}
 resource "null_resource" "test7" {}
 
+resource "null_resource" "test8" {}
+
+
 variable "website" {}
 # comment
 check "health_check" {
